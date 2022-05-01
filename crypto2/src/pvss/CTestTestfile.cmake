584da68cb3f6162d7e5100f102b0f55b7e0660e6
mastran <mastran.nibesh@yahoo.com>
2022-05-01 11:10:24 -0400

    remove some make files

--- crypto2/src/pvss/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nibesh/Code/optrand/crypto2/src/pvss
-# Build directory: /home/nibesh/Code/optrand/crypto2/src/pvss
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
